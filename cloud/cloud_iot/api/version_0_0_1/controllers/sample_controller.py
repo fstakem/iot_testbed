7d60e1eddbcaf68782b6ff1c91576b0e23dd610d
Fredrick Stakem <fstakem@gmail.com>
2017-07-03 09:04:48 -0400

    Finish data upload

--- cloud/cloud_iot/api/version_0_0_1/controllers/sample_controller.py
+++ cloud/cloud_iot/api/version_0_0_1/controllers/sample_controller.py
@@ -9,8 +9,10 @@
 
 # Libraries
 from flask import jsonify
+from datetime import datetime
 from flask_restful import Resource, reqparse, abort, Api
 
+from cloud_iot.db.models.api import Session
 from cloud_iot.db.models.sensor import Sensor
 from cloud_iot.db.models.sample import Sample
 
@@ -43,7 +45,26 @@ class SampleListController(Resource):
     def post(self):
         args = SampleListController.parser.parse_args()
         sensor_id = int(args['sensor_id'])
-        sampled_at = str(args['sampled_at'])
-        value = str(args['value'])
+        sampled_at = None
+        value = None
+
+        session = Session()
+        sensor = session.query(Sensor).get(sensor_id)
+
+        if sensor:
+            sampled_at_str = str(args['sampled_at'])
+            try:
+                sampled_at = datetime.strptime(sampled_at_str, "%Y-%m-%d %H:%M:%S")
+                value = str(args['value'])
+
+                sample = Sample()
+                sample.sampled_at = sampled_at
+                sample.value = value
+                sensor.samples.append(sample)
+                session.add(sensor)
+                session.add(sample)
+                session.commit()
+            except ValueError as err:
+                pass
 
         return jsonify(id=sensor_id, sampled_at=sampled_at, value=value)
